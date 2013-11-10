040dcff4f6665771c609039a3d4acab6ca5bf84a
Erik Boyle <erikboyle@me.com>
2013-11-10 09:56:59 -0600

    added new sqlite project

---
+++ 340_sqlite/340_sqlite.pro
@@ -0,0 +1,24 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-11-10T07:40:04
+#
+#-------------------------------------------------
+
+QT       += core sql
+
+QT       -= gui
+
+
+TARGET = 340_sqlite
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += \
+    BusinessLayer.cpp \
+    DataAccessLayer.cpp \
+    PresentationLayer.cpp
+
+HEADERS +=
---
+++ 340_sqlite/PresentationLayer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <QCoreApplication>
+#include <QtSql>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("Netflix.sqlite");
+    bool ok = db.open();
+    if (!ok) {
+        cout << "Error opening database" << endl;
+        exit(1);
+    }
+
+    QSqlQuery query(db);
+    ok = query.exec("SELECT Count(*) FROM Movies");
+    if (!ok) {
+        cout << "exec error" << endl;
+        exit(1);
+    }
+    query.next();
+    int count = query.value(0).toInt();
+    cout << "count = " << count << endl;
+    /*
+    while (query.next()) {
+        //QString name = query.value(0).toString();
+        int count = query.value(0).toInt();
+        cout << "count = " << count << endl;
+        //int salary = query.value(1).toInt();
+        //qDebug() << name << salary;
+    }
+    */
+    return a.exec();
+}
