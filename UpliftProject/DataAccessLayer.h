4858285b8d0fd68436abb0e5d6e0b47f873be193
maximusNullus <verc84@gmail.com>
2013-11-28 20:11:51 -0600

    Still fixing the primary key mess.  Making our primary key fields auto increment in the creation schema.

--- UpliftProject/DataAccessLayer.h
+++ UpliftProject/DataAccessLayer.h
@@ -33,25 +33,25 @@ public:
         bool success;
 
         //This table is the only place that the exercise name needs to exist.  All other locations should only have the id number (in case of edits to the name).
-        success = query.exec("CREATE TABLE IF NOT EXISTS exercise_table (exercise_name_id INT PRIMARY KEY, exercise_name TEXT)");
+        success = query.exec("CREATE TABLE IF NOT EXISTS exercise_table (exercise_name_id INTEGER PRIMARY KEY, exercise_name TEXT)");
         if (!success) {
             qDebug("Error creating exercise_table");
             qDebug("%s", qPrintable(db.lastError().text()));
             //exit(0);
         }
-        success = query.exec("CREATE TABLE IF NOT EXISTS workout_table (workout_name_id INT PRIMARY KEY, workout_name TEXT)");
+        success = query.exec("CREATE TABLE IF NOT EXISTS workout_table (workout_name_id INTEGER PRIMARY KEY, workout_name TEXT)");
         if (!success) {
             qDebug("Error creating exercise_table");
             qDebug("%s", qPrintable(db.lastError().text()));
             //exit(0);
         }
-        success = query.exec("CREATE TABLE IF NOT EXISTS workout_pairs (workout_id INT, exercise_id INT, workoutOrder INT)");
+        success = query.exec("CREATE TABLE IF NOT EXISTS workout_pairs (workout_id INTEGER, exercise_id INTEGER, workoutOrder INTEGER)");
         if (!success) {
             qDebug("Error creating exercise_table");
             qDebug("%s", qPrintable(db.lastError().text()));
             //exit(0);
         }
-        success = query.exec("CREATE TABLE IF NOT EXISTS user_table (user_id INT PRIMARY KEY, username TEXT, password TEXT)");
+        success = query.exec("CREATE TABLE IF NOT EXISTS user_table (user_id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
         if (!success) {
             qDebug("Error creating exercise_table");
             qDebug("%s", qPrintable(db.lastError().text()));
@@ -59,14 +59,14 @@ public:
         }
         //--->insert volume into here eventually?
         //Not necessarily.  We can just run a count query on the numebr of logs, selecting by user_id.
-        success = query.exec("CREATE TABLE IF NOT EXISTS workout_log (workout_instance_id INT PRIMARY KEY, user_id INT, workout_name_id int, time timestamp default (strftime('%s', 'now')))");
+        success = query.exec("CREATE TABLE IF NOT EXISTS workout_log (workout_instance_id INTEGER PRIMARY KEY, user_id INTEGER, workout_name_id INTEGER, time timestamp default (strftime('%s', 'now')))");
         if (!success) {
             qDebug("Error creating exercise_table");
             qDebug("%s", qPrintable(db.lastError().text()));
             //exit(0);
         }
         //don't pass in anything for set_id or time (especially not NULL). It will auto inc set_id and stamp time by default
-        success = query.exec("CREATE TABLE IF NOT EXISTS exercise_set_log (set_id INT PRIMARY KEY, workout_id INT, exercise_id INT, user_id INT, time timestamp default (strftime('%s', 'now')), reps INT, weight INT, one_rep_max INT)");
+        success = query.exec("CREATE TABLE IF NOT EXISTS exercise_set_log (set_id INTEGER PRIMARY KEY, workout_id INTEGER, exercise_id INTEGER, user_id INTEGER, time timestamp default (strftime('%s', 'now')), reps INTEGER, weight INTEGER, one_rep_max INTEGER)");
         if (!success) {
             qDebug("Error creating exercise_table");
             qDebug("%s", qPrintable(db.lastError().text()));
