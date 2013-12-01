1e5116c42335cf2e4719663ff5eca090d7611caf
maximusNullus <verc84@gmail.com>
2013-11-30 21:24:26 -0600

    Stat functions finished, ready for testing.

--- UpliftProject/ExerciseStats.cpp
+++ UpliftProject/ExerciseStats.cpp
@@ -13,15 +13,74 @@ int getFirstToLastWorkout(int user_id){
         return ((result.value(0).toInt())/60/60/24);
     }
     return -1; //failed*/
-    ;
 }
 
-int getWorkoutFrequency(int user_id);
+int getTotalNumOfWorkouts(int user_id){
+    QString command = "SELECT count(*) FROM workout_log WHERE user_id == '" + QString::number(user_id) + "')";
+    QSqlQuery result = dt->executeQuery(command);
 
-int getTotalNumOfWorkouts(int user_id);
+    if (result.next()){
+        return result.value(0).toInt();
+    }
+    return -1; //failed*/
+}
 
-int getTotalNumOfSets(int user_id);
+int getWorkoutFrequency(int user_id){
+    int workoutNum = getTotalNumOfWorkouts(user_id);
+    int dayNum = getFirstToLastWorkout(user_id);
 
-int getAvgSetsPerWorkout(int user_id);
+    if(workoutNum < 0){
+        return -1;  //workoutNum failure.
+    }
+    if(dayNum < 0){
+        return -1;  //dayNum failure.
+    }
 
-int getAvgRepsPerSet(int user_id);
+    if (dayNum > 0){  //avoid divide by zero errors.
+        return workoutNum/dayNum;
+    }
+    return -1; //failed*/
+}
+
+int getTotalNumOfSets(int user_id){
+    QString command = "SELECT count(*) FROM exercise_set_log WHERE user_id == '" + QString::number(user_id) + "')";
+    QSqlQuery result = dt->executeQuery(command);
+
+    if (result.next()){
+        return result.value(0).toInt();
+    }
+    return -1; //failed*/
+}
+
+int getAvgSetsPerWorkout(int user_id){
+    int setNum = getTotalNumOfSets(user_id);
+    int workoutNum = getTotalNumOfWorkouts(user_id);
+
+    if(setNum < 0){
+        return -1; //setNum failed.
+    }
+    if(workoutNum < 0){
+        return -1; //workoutNum failed.
+    }
+
+    if(workoutNum > 0) {  //avoid divide by zero problems.
+        return setNum/workoutNum;
+    }
+    return -1; //failed*/
+}
+
+int getAvgRepsPerSet(int user_id){
+    int setNum = getTotalNumOfSets(user_id);
+    if (setNum < 0){
+        return -1;  //setNum failure.
+    }
+    QString command = "SELECT SUM(reps) FROM exercise_set_log WHERE user_id == '" + QString::number(user_id) + "')";
+    QSqlQuery result = dt->executeQuery(command);
+
+    if (result.next()){
+        if(setNum > 0){  //avoid divide by zero error
+            return (result.value(0).toInt())/setNum;
+        }
+    }
+    return -1; //failed*/
+}
