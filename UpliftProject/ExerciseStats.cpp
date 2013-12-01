1bfcd948c277715e09b7e4c57e1961466fb97067
maximusNullus <verc84@gmail.com>
2013-11-30 20:58:13 -0600

    getFirstToLastWorkout finished.

--- UpliftProject/ExerciseStats.cpp
+++ UpliftProject/ExerciseStats.cpp
@@ -1,14 +1,16 @@
 #include "ExerciseStats.h"
+#include <QString>
+#include <QSqlQuery>
 
 //Stubs created.
 
 
 int getFirstToLastWorkout(int user_id){
-/*    QString command = "SELECT workout_name_id FROM workout_table WHERE workout_name == '" + name + "' AND user_id == '" + QString::number(user_id) + "'";
+    QString command = "SELECT (SELECT time FROM workout_log WHERE user_id == '" + QString::number(user_id) + "' ORDER BY time DESC LIMIT 1) - (SELECT time FROM workout_log WHERE user_id == '" + QString::number(user_id) + "' ORDER BY time ASC LIMIT 1)";
     QSqlQuery result = dt->executeQuery(command);
 
     if (result.next()) {
-        return result.value(0).toInt();
+        return ((result.value(0).toInt())/60/60/24);
     }
     return -1; //failed*/
     ;
