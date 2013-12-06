81dc9d1a81b61d2c982707a701db9b3b7e804e35
Erik Boyle <erikboyle@me.com>
2013-12-05 18:23:51 -0600

    removed pointless comments

--- UpliftProject/main.cpp
+++ UpliftProject/main.cpp
@@ -10,21 +10,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    /*
-    BusinessTier *bt = new BusinessTier();
-    bt->AddExercise("Squat");
-    bt->AddExercise("bench press");
-    bt->RemoveExercise("Squat");
-    bt->RemoveExercise("bench press");
-    bt->AddUser("erikboyle", "pass123");
-    bt->RemoveUser("erikboyle", "pass123");
-
-    bt->AddWorkout("All pro");
-    bt->AddWorkout("Chest/Back A");
-
-    bt->GetWorkoutList();
-    */
-
     Widget w;
     w.show();
 
